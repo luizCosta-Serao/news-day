f716d890c358df0c4e80871d348a2e92bd7e10d0
Luiz Serão <luiz.serao@hotmail.com>
2024-06-20 07:26:39 -0300

    header

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
+<?php include('config.php') ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>css/style.css">
   <title>Portal de Notícias</title>
 </head>
 <body>
-
+  <header class="header">
+      <a class="logo" href="<?php echo INCLUDE_PATH; ?>">
+          <img src="<?php echo INCLUDE_PATH; ?>assets/logo.svg" alt="">
+      </a>
+      <nav class="menu">
+          <ul>
+              <li><a href="<?php echo INCLUDE_PATH; ?>">Home</a></li>
+              <li><a href="">Recentes</a></li>
+              <li><a href="">Esportes</a></li>
+              <li><a href="">Gerais</a></li>
+              <li><a href="">Saúde</a></li>
+          </ul>
+      </nav>
+  </header>
 </body>
 </html>
