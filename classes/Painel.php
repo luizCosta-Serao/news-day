943bcebe19d822ed848fbb0f97792073dfbaf139
Luiz Serão <luiz.serao@hotmail.com>
2024-06-21 08:48:27 -0300

    cadastrar-categoria

--- classes/Painel.php
+++ classes/Painel.php
@@ -21,6 +21,59 @@
         include('pages/home.php');
       }
     }
+
+    public static function alert($type, $message) {
+      if ($type === 'sucesso') {
+        echo '<p class="sucesso">'.$message.'</p>';
+      } else if ($type === 'erro') {
+        echo '<p class="erro">'.$message.'</p>';
+      }
+    }
+
+     // Gerador de slug
+     public static function generateSlug($str) {
+      $str = mb_strtolower($str);
+      $str = preg_replace('/(â|á|ã)/', 'a', $str);
+      $str = preg_replace('/(ê|é)/', 'e', $str);
+      $str = preg_replace('/(í|Í)/', 'i', $str);
+      $str = preg_replace('/(ú)/', 'u', $str);
+      $str = preg_replace('/(ó|ô|õ|Ô)/', 'o', $str);
+      $str = preg_replace('/(_|\/|!|\?|#)/', '', $str);
+      $str = preg_replace('/( )/', '-', $str);
+      $str = preg_replace('/ç/', 'c', $str);
+      $str = preg_replace('/(-[-]{1,})/', '-', $str);
+      $str = preg_replace('/(,)/', '-', $str);
+      $str = strtolower($str);
+      return $str;
+    }
+
+    public static function insertInDatabase($arr) {
+      $certo = true;
+      // obtendo o nome da tabela através do $_POST de um input:hidden
+      $nome_tabela = $arr['nome_tabela'];
+      // query para inserção dos dados no banco de dados
+      $query = "INSERT INTO `$nome_tabela` VALUES (null";
+        foreach ($arr as $key => $value) {
+          $nome = $key;
+          $valor = $value;
+          if ($nome === 'action' || $nome === 'nome_tabela') {
+            continue;
+          }
+          if ($value === '') {
+            $certo = false;
+            break;
+          }
+          $query.=",?";
+          $parametros[] = $value;
+        }
+      $query.=")";
+      //Fim da query para inserção dos dados no banco de dados
+      if ($certo === true) {
+        $sql = MySql::conectar()->prepare($query);
+        $sql->execute($parametros);
+      }
+      return $certo;
+    }
   }
 
 ?>
