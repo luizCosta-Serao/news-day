b1c2133043de48468b7c08774364e332fac9a772
Luiz Serão <luiz.serao@hotmail.com>
2024-06-21 06:39:53 -0300

    html e css inicial do painel de controle

--- painel/main.php
+++ painel/main.php
@@ -1,1 +1,20 @@
-Bem-vindo ao painel de controle <?php echo $_SESSION['name']; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo INCLUDE_PATH_PAINEL ?>css/style.css">
+  <title>Document</title>
+</head>
+<body>
+  <header class="header">
+    <a href="">Sair</a>
+  </header>
+  <section class="submenu">
+    <a href="<?php echo INCLUDE_PATH_PAINEL; ?>cadastrar-categoria">Cadastrar Categoria |</a>
+    <a href="<?php echo INCLUDE_PATH_PAINEL; ?>gerenciar-categorias">Gerenciar Categorias |</a>
+    <a href="<?php echo INCLUDE_PATH_PAINEL; ?>cadastrar-noticia">Cadastrar Notícia |</a>
+    <a href="<?php echo INCLUDE_PATH_PAINEL; ?>gerenciar-noticias">Gerenciar Noticías |</a>
+  </section>
+</body>
+</html>
